3cb08f5e56234a4853c72d739a987273ec836514
James Lucas <jiml95338@gmail.com>
2024-12-10 11:52:48 -0800

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SmartFarmGame from './components/SmartFarmGame';
+
+function App() {
+  return (
+    <div className="App">
+      <SmartFarmGame />
+    </div>
+  );
+}
+
+export default App;
